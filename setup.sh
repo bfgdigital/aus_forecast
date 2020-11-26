37fd75e43ab5a91c574b328f401fc05f5eefd128
bfgdigital <ben@bfg.digital>
2020-11-26 14:46:21 +1100

    updated packages

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 mkdir -p ~/.streamlit/
-echo "
-[server]\n
-headless = true\n
-enableCORS=false\n
-port = $PORT\n
-\n
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
